c97433e88aa511e671008a08c68d5fea139785ed
dPanibra0 <dpanibra0@gmail.com>
2024-03-30 14:03:25 -0500

    add: register ui page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {MatInputModule, MatLabel} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatInputModule, MatLabel, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators, ReactiveFormsModule, NgForm, FormGroupDirective } from '@angular/forms';
+import Validation from '../../utils/validation';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
-  templateUrl: './login.component.html'
+  imports: [ReactiveFormsModule, CommonModule, MatFormFieldModule, MatInputModule],
+  templateUrl: './login.component.html',
 })
-export default class LoginComponent {
 
+export default class LoginComponent implements OnInit{
+  // form: FormGroup = new FormGroup({
+  //   firstName: new FormControl(''),
+  //   lastName: new FormControl(''),
+  //   email: new FormControl<String>(''),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''),
+  //   acceptTerms: new FormControl(false),
+  // });
+  form: FormGroup = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    career: new FormControl(''),
+    phone: new FormControl(''),
+    email: new FormControl(''),
+    activities: new FormControl(),
+  });
+
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void{
+    this.form = this.formBuilder.group(
+      {
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        career: ['', Validators.required],
+        phone: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        // password: [
+        //   '',
+        //   [
+        //     Validators.required,
+        //     Validators.minLength(6),
+        //     Validators.maxLength(40)
+        //   ]
+        // ],
+        // confirmPassword: ['', Validators.required],
+      }
+      // {
+      //   validators: [Validation.match('password', 'confirmPassword')]
+      // }
+    );
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.form.invalid) {
+      return;
+    }
+
+    console.log(JSON.stringify(this.form.value, null, 2));
+  }
+
+  onReset(): void {
+    this.submitted = false;
+    this.form.reset();
+  }
 }
