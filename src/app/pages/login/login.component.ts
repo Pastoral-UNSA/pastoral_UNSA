e5ad2f82a81f7467a924c535abc5e8ed0ae424aa
dPanibra0 <dpanibra0@gmail.com>
2024-04-01 22:35:02 -0500

    add: login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,63 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 
-import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators, ReactiveFormsModule, NgForm, FormGroupDirective } from '@angular/forms';
-import Validation from '../../utils/validation';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
-
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './login.component.html',
 })
-
-export default class LoginComponent implements OnInit{
-  // form: FormGroup = new FormGroup({
-  //   firstName: new FormControl(''),
-  //   lastName: new FormControl(''),
-  //   email: new FormControl<String>(''),
-  //   password: new FormControl(''),
-  //   confirmPassword: new FormControl(''),
-  //   acceptTerms: new FormControl(false),
-  // });
+export default class LoginComponent implements OnInit {
   form: FormGroup = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    career: new FormControl(''),
-    phone: new FormControl(''),
-    email: new FormControl(''),
-    activities: new FormControl(),
+    user: new FormControl(''),
+    password: new FormControl(''),
   });
 
   submitted = false;
 
   constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit(): void{
-    this.form = this.formBuilder.group(
-      {
-        firstName: ['', Validators.required],
-        lastName: ['', Validators.required],
-        career: ['', Validators.required],
-        phone: ['', Validators.required],
-        email: ['', [Validators.required, Validators.email]],
-        // password: [
-        //   '',
-        //   [
-        //     Validators.required,
-        //     Validators.minLength(6),
-        //     Validators.maxLength(40)
-        //   ]
-        // ],
-        // confirmPassword: ['', Validators.required],
-      }
-      // {
-      //   validators: [Validation.match('password', 'confirmPassword')]
-      // }
-    );
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      user: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
   }
 
   get f(): { [key: string]: AbstractControl } {
